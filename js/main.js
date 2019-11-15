290a401d402f1167f763b2ae697d911d77abc763
jpgr4 <42671904+jpgr4@users.noreply.github.com>
2019-11-16 06:50:11 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-
-if (response.ok) { // if HTTP-status is 200-299
-  // get the response body (the method explained below)
-  let json = await response.json();
-  console.log(json);
-} else {
-  alert("HTTP-Error: " + response.status);
-}
+$(document).ready(function(){
+  console.log(1);
+  $.get('https://jsonplaceholder.typicode.com/todos/1', data, function() {
+     console.log(data);
+  });
+});
