9b632371a64ca3b5d5c4d85406f65bf24aabb80f
jpgr4 <42671904+jpgr4@users.noreply.github.com>
2019-11-16 06:47:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let response = await fetch(url);
+let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
 
 if (response.ok) { // if HTTP-status is 200-299
   // get the response body (the method explained below)
